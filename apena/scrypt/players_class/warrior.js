fe21b3c5d6e79eb4a7a95fdf88d67080bb6f4754
Alexeykoh <Alexeykoh@icloud.com>
2023-12-20 19:30:29 +0300

    feat: add image link

--- apena/scrypt/players_class/warrior.js
+++ apena/scrypt/players_class/warrior.js
@@ -1,11 +1,11 @@
-export var warrior = {
+export const warrior = {
+	img: "./assets/main_assets/warrior1.jpg",
 	class: "Warrior",
 	health: 100,
 	mana: 100,
 	physical_def: 10,
 	magical_def: 5,
-    skills: {
-        first_attack: {},
-
-    },
+	skills: {
+		first_attack: {},
+	},
 };
