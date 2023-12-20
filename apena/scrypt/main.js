8868a6d11922205ac9a7ec3c858037bb3b6897e2
Alexeykoh <Alexeykoh@icloud.com>
2023-12-20 19:29:03 +0300

    feat: add helper function

--- apena/scrypt/main.js
+++ apena/scrypt/main.js
@@ -1,3 +1,11 @@
 import { render_stats } from "./player/player.js";
 
+export function appendAll(rootNode, arrayOnNodes) {
+	arrayOnNodes.forEach((el) => {
+		rootNode.appendChild(el);
+	});
+	return rootNode;
+}
+
+console.log(Node);
 render_stats();
