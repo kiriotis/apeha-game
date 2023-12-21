61e1341579b8d63f80ecc6ef2589ab34f754ecfe
Alexeykoh <Alexeykoh@icloud.com>
2023-12-21 14:18:32 +0300

    refactor: replace appendAll

--- apena/scrypt/main.js
+++ apena/scrypt/main.js
@@ -1,11 +1,3 @@
 import { render_stats } from "./player/player.js";
-
-export function appendAll(rootNode, arrayOnNodes) {
-	arrayOnNodes.forEach((el) => {
-		rootNode.appendChild(el);
-	});
-	return rootNode;
-}
-
-console.log(Node);
+//
 render_stats();
