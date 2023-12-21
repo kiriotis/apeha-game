0385db647aea393d6d4e98fc1f5f76a35bf97fa9
Alexeykoh <Alexeykoh@icloud.com>
2023-12-21 14:20:51 +0300

    refactor: edit render mechanism and add docs

--- apena/scrypt/player/player.js
+++ apena/scrypt/player/player.js
@@ -1,52 +1,57 @@
-import { core_battle } from "./../battle/battle.js";
-import { player1_stack } from "./../battle/battle.js";
-import { player2_stack } from "./../battle/battle.js";
+import { core_battle, player1_stack } from "./../battle/battle.js";
 import { popUP_render } from "./popUP.js";
-
-let left_part_skils = document.querySelector(".main_left");
-let right_part_skils = document.querySelector(".main_right");
 
 ////////////////////////////////////////////////////////////////
 
+// первичный рендер
 popUP_render();
 
 /////////////////////////////////////////////////////////////////
-export function render_stats(player1_choiseIS, player2_choiseIS) {
-	console.log(player1_choiseIS);
-
-	let player1_title = document.querySelector(".player1_title");
-	player1_title.innerHTML = player1_choiseIS.class;
-	let player_hp = document.querySelector(".player_hp");
-	player_hp.innerHTML = `${player1_choiseIS.health}` + " Hp";
-	let player_mp = document.querySelector(".player_mp");
-	player_mp.innerHTML = `${player1_choiseIS.mana}` + " Mp";
-
-	for (const key in player1_choiseIS.skills) {
-		const skill_btn = document.createElement("button");
-		skill_btn.className = key;
-		skill_btn.addEventListener("click", () => {
-			player1_stack({ ...player1_choiseIS.skills[key], name: key });
-		});
-		skill_btn.innerHTML = key;
-		left_part_skils.appendChild(skill_btn);
+// рендер данных о выбранных классах
+export function render_stats(player1_choiceIS, player2_choiceIS) {
+	if (!player1_choiceIS | !player2_choiceIS) {
+		// рендер отменяется если ни один из выборов не сделан
+		return;
 	}
-
-	//////////////
-	let player2_title = document.querySelector(".player2_title");
-	player2_title.innerHTML = player2_choiseIS.class;
-	let player2_hp = document.querySelector(".player2_hp");
-	player2_hp.innerHTML = `${player2_choiseIS.health}` + " Hp";
-	let player2_mp = document.querySelector(".player2_mp");
-	player2_mp.innerHTML = `${player2_choiseIS.mana}` + " Mp";
-	for (const key in player2_choiseIS.skills) {
-		const skill_btn = document.createElement("button");
-		skill_btn.className = key;
-		skill_btn.addEventListener("click", () => {
-			player2_stack({ ...player2_choiseIS.skills[key], name: key });
+	//
+	// выборы объединяются в общий массив
+	const playersChoice = [player1_choiceIS, player2_choiceIS];
+	//
+	// запись данных на основе выбора класса через перебор массива
+	// данные подставляются по индексу
+	playersChoice.forEach((player, index) => {
+		//
+		// запись названия класса
+		const title = document.querySelector(`.player_${index + 1}_title`);
+		title.innerHTML = player.class;
+		// запись картинки
+		const img = document.querySelector(
+			`.player_${index + 1}_img_wrapper>img`
+		);
+		img.src = player.img;
+		// запись HP
+		const hp = document.querySelector(`.player_${index + 1}_hp`);
+		hp.innerHTML = `${player.health}` + " Hp";
+		// запись MP
+		const mp = document.querySelector(`.player_${index + 1}_mp`);
+		mp.innerHTML = `${player.mana}` + " Mp";
+		//
+		let skillButtonContainer = document.querySelector(
+			`.player_${index + 1}_skills`
+		);
+		//
+		player.skills.forEach((skill) => {
+			const skill_btn = document.createElement("button");
+			//
+			skill_btn.className = skill.name;
+			skill_btn.innerHTML = skill.name;
+			skill_btn.addEventListener("click", () => {
+				player1_stack(skill);
+			});
+			skillButtonContainer.appendChild(skill_btn);
 		});
-		skill_btn.innerHTML = key;
-		right_part_skils.appendChild(skill_btn);
-	}
-	////////////////////////
-	core_battle(player1_choiseIS, player2_choiseIS);
+	});
+	//
+	// вызов механики боя с передачей туда выбранных классов
+	core_battle(player1_choiceIS, player2_choiceIS);
 }
