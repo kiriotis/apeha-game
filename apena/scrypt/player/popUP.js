c02cf53ee2170c0c280c2aaa9b2ca6762da778ce
Alexeykoh <Alexeykoh@icloud.com>
2023-12-21 15:02:56 +0300

    feat: add selection changed фтв вщсы

--- apena/scrypt/player/popUP.js
+++ apena/scrypt/player/popUP.js
@@ -6,15 +6,26 @@ let popUp = document.querySelector(".popUP_container");
 //
 let chosen_card = null;
 //
+// контейнер с выбором
 let player1_choiceIS;
 let player2_choiceIS;
 
 //
-document.getElementById("confirm").addEventListener("click", checked_player);
+// кнопка подтверждения
+const confirmBtn = document.getElementById("confirm");
+confirmBtn.addEventListener("click", checked_player);
+// по умолчанию выключена
+confirmBtn.disabled = true;
 //
+// показывает кто сейчас выбирает класс
+const whoSelect = document.querySelector(".whoSelect");
+whoSelect.innerHTML = "player 1";
 
+//
+// изменяет состояние выбранной карты в меню выбора
 function setChoice(cardData) {
 	chosen_card = cardData;
+	confirmBtn.disabled = false;
 }
 
 ///////////////////////////////////////////////////////
@@ -30,15 +41,37 @@ export function popUP_render(data_card) {
 ///////////////////////////////////////////////////////////
 
 function checked_player() {
+	confirmBtn.disabled = true;
+	//
 	document.querySelectorAll(".img_popup_wrapper_pre").forEach((el) => {
 		el.classList.remove("active");
 	});
 	if (player1_choiceIS == undefined) {
 		player1_choiceIS = chosen_card;
+		//
+		// анимация
+		popUp.style.opacity = "0%";
+		setTimeout(() => {
+			popUp.style.opacity = "100%";
+		}, 150);
 	} else {
 		player2_choiceIS = chosen_card;
-		popUp.classList.add("popUP_Disable");
+		//
+		// анимация
+		popUp.style.opacity = "0%";
+		setTimeout(() => {
+			popUp.classList.add("popUP_Disable");
+		}, 150);
 		render_stats(player1_choiceIS, player2_choiceIS);
+	}
+	//
+	// меняет того. кто выбирает
+	if (player1_choiceIS) {
+		setTimeout(() => {
+			whoSelect.innerHTML = "player 2";
+		}, 150);
+	} else {
+		whoSelect.innerHTML = "player 1";
 	}
 }
 
