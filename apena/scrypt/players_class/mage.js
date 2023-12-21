079c765d367e0b262a09210600c626c4b0cb295b
Alexeykoh <Alexeykoh@icloud.com>
2023-12-21 14:24:15 +0300

    refactor: сhanged the way the class is created

--- apena/scrypt/players_class/mage.js
+++ apena/scrypt/players_class/mage.js
@@ -1,12 +1,18 @@
-export const mage = {
+import { createClass } from "../helpers/createClass.js";
+import skillsList from "./skills/skillsList.js";
+import playerClasses from "./types/classTypes.js";
+
+//
+const mage = new createClass({
 	img: "./assets/main_assets/warrior2.jpg",
-	class: "Mage",
+	class: playerClasses.wizard,
 	health: 75,
 	mana: 125,
 	physical_def: 5,
 	magical_def: 10,
-	skills: {
-		fire_ball: { type: "magic", damage: 10 },
-		earth_ball: { type: "combine", damage: 10, mag_damge: 10, },
-	},
-};
+	skills: [skillsList["fire_ball"]],
+});
+//
+Object.freeze(mage);
+//
+export default mage;
--- apena/scrypt/players_class/warrior.js
+++ apena/scrypt/players_class/warrior.js
@@ -1,18 +1,18 @@
-export const warrior = {
+import { createClass } from "../helpers/createClass.js";
+import skillsList from "./skills/skillsList.js";
+import playerClasses from "./types/classTypes.js";
+
+//
+const warrior = new createClass({
 	img: "./assets/main_assets/warrior1.jpg",
-	class: "Warrior",
-	health: 100,
+	class: playerClasses.warrior,
+	health: 120,
 	mana: 100,
 	physical_def: 10,
 	magical_def: 5,
-	skills: {
-		"Normal Attack": {
-			type: "normal",
-			damage: 10,
-		},
-		powerful_attack: {
-			type: "normal",
-			damage: 20,
-		},
-	},
-};
+	skills: [skillsList["normalAttack"], skillsList["powerfulAttack"]],
+});
+//
+Object.freeze(warrior);
+//
+export default warrior;
